3599ec4e7adab74b966116bcd107d569345ebb51
Caleb King <calebking9876@gmail.com>
2026-03-11 11:50:47 +1300

    Add analysis function for unprocessed duplicated rows
    This function generates visualizations for analyzing unprocessed duplicated rows in a dataset, including a line chart for dataset size and a bar chart for duplicate counts.

---
+++ Functions/Analysis/analyze_duplicated_rows_unprocessed.py
@@ -0,0 +1,24 @@
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# DUPLICATED ROWS UNPROCESSED ANALYSIS
+def analyze_duplicated_rows_unprocessed(unprocessed_duplicates_ds, duplicates_unprocessed):
+
+  # line chart showing dataset size - (left)
+  x_1 = np.arange(unprocessed_duplicates_ds)
+  plt.figure(figsize=(9, 4)) # sets size of line charts
+  plt.subplot(1, 2, 1) # position of linechart
+  plt.plot(x_1, x_1, color='blue', label=f"Rows {unprocessed_duplicates_ds:,}")
+  plt.title("Unprocessed - Dataset Size")
+  plt.legend()
+
+  # bar chart showing duplicate rows - (right)
+  plt.subplot(1, 2, 2)
+  labels = [f'Duplicates ({duplicates_unprocessed:,})']
+  values = [duplicates_unprocessed]
+  mycolors = ["green"]
+  plt.bar(labels, values, color=mycolors)
+  plt.title('Duplicates Found')
+  plt.tight_layout()
+  plt.show()
