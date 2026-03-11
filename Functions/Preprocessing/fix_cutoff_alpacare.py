3c7461b2016836b7d8bbf6aaea112f2ae59e9dcf
Caleb King <calebking9876@gmail.com>
2026-03-12 09:03:49 +1300

    Update fix_cutoff_alpacare.py

--- Functions/Preprocessing/fix_cutoff_alpacare.py
+++ Functions/Preprocessing/fix_cutoff_alpacare.py
@@ -21,7 +21,7 @@ def fix_cutoff_alpacare(processed_filtered_df):
   )
 
   # sum of rows found
-  cut_off_count = is_cut_off.sum()
+  cut_off_unprocessed_count = is_cut_off.sum()
 
   # gets the index of the rows
   indices_to_drop = is_cut_off.index[is_cut_off]
@@ -38,5 +38,11 @@ def fix_cutoff_alpacare(processed_filtered_df):
   # gets the length of the unprocessed dataframe processed
   processed_cutoff_ds = len(processed_alpa_df)
 
+ # find rows which are cut off and over 1300 characters
+  is_cut_off_final = unprocessed_alpa_df.loc[alpac_subset, 'output'].apply(
+      lambda x: (len(re.findall(r'(?i)([.?\"\]!\)]\s*$)', str(x))) == 0) and (len(str(x)) > 1200)
+  )
+  cut_off_processed_count = is_cut_off.sum()
+
   # return the variables that are used inside this function
-  return(unprocessed_cutoff_ds, unprocessed_alpa_ds, processed_alpa_ds, cut_off_count, processed_cutoff_ds, processed_alpa_df)
+  return(unprocessed_cutoff_ds, unprocessed_alpa_ds, processed_alpa_ds, cut_off_unprocessed_count, processed_cutoff_ds, cut_off_processed_count, processed_alpa_df)
