df8cabf46be91a6124166a00502f9c6fdff60e97
Caleb King <calebking9876@gmail.com>
2026-03-11 14:04:46 +1300

    Create add_fullstops_pubcasual_analysis.py

---
+++ Functions/Analysis/add_fullstops_pubcasual_analysis.py
@@ -0,0 +1,35 @@
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# FULLSTOP UNPROCESSED / PROCESSED ANALYSIS
+def add_fullstops_pubcasual_analysis(processed_fs_ds, fs_count_unprocessed, fs_count_processed):
+
+  # line chart showing subset pubmed size - (left)
+  x_1 = np.arange(processed_fs_ds)
+  plt.figure(figsize=(9, 4)) # sets size of line charts
+  plt.subplot(1, 2, 1) # position of linechart
+  plt.plot(x_1, x_1, color='red', label=f"Rows {processed_fs_ds:,}")
+  plt.title(f"Processed - PubMed Subset Size")
+  plt.legend()
+
+  # bar chart showing fullstops missing unprocessed - (right)
+  plt.subplot(1, 2, 2) 
+  labels = [f'Missing ({fs_count_unprocessed:,})']
+  values = [fs_count_unprocessed]
+  mycolors = ["red"]
+  plt.bar(labels, values, color=mycolors)
+  plt.title('Unprocessed Output Missing Full Stops')
+  plt.ylim(0)
+  plt.tight_layout()
+  plt.show()
+
+  # bar chart showing fullstops missing processed - (bottom)
+  plt.figure(figsize=(10.5, 4))
+  labels = [f'Missing ({fs_count_processed:,})']
+  values = [fs_count_processed]
+  mycolors = ["purple"]
+  plt.title('Processed Output Missing Full Stops')
+  plt.bar(labels, values, color=mycolors)
+  plt.ylim(0)
+  plt.show()
