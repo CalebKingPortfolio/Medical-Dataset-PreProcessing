151c11e06eb1cc8985816acc29edf15b5ce6819c
Caleb King <calebking9876@gmail.com>
2026-03-11 16:24:40 +1300

    Create filter_rows_by_length.py

---
+++ Functions/Preprocessing/filter_rows_by_length.py
@@ -0,0 +1,37 @@
+def filter_rows_by_length(processed_spaces_df):
+
+  # minumin and maximum row size
+  min_total_chars = 50
+  max_output_chars = 2000
+  min_output_chars = 5 
+
+  # copy of the dataframe
+  unprocessed_filtered_df = processed_spaces_df.copy()
+
+  # gets the original size of the dataset
+  unprocessed_filtered_ds = len(unprocessed_filtered_df)
+
+  # count length per row
+  unprocessed_filtered_df['total_len'] = (
+      unprocessed_filtered_df['instruction'].astype(str).str.len() +
+      unprocessed_filtered_df['input'].astype(str).str.len() +
+      unprocessed_filtered_df['output'].astype(str).str.len()
+  )
+
+  # keep rows with total_len >= min, output length <= max, and output length > min
+  unprocessed_filtered_df = unprocessed_filtered_df[
+      (unprocessed_filtered_df['total_len'] >= min_total_chars) &
+      (unprocessed_filtered_df['output'].astype(str).str.len() <= max_output_chars) &
+      (unprocessed_filtered_df['output'].astype(str).str.len() > min_output_chars)
+  ]
+
+  # drops the column total_len
+  processed_filtered_df = unprocessed_filtered_df.drop(columns=['total_len'])
+
+  # gets the original size of the dataset
+  processed_filtered_ds = len(processed_filtered_df)
+
+  # total number of rows droped
+  total_droped = unprocessed_filtered_ds - processed_filtered_ds
+  
+  return(unprocessed_filtered_ds, processed_filtered_ds, total_droped, processed_filtered_df)
